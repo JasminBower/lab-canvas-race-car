1a1cf1d3930072b706c3addd4be338d29a54fd7c
JasminBower <jasmin.ibower@gmail.com>
2022-02-25 08:50:48 +0100

    Thurs Lab

--- js/index.js
+++ js/index.js
@@ -1,7 +1,67 @@
+const splashScreen = document.querySelector('.game-intro');
+
+let bg;
+let y = 0;
+let car;
+let img;
+
+function preload(){
+   bg = loadImage('../images/road.png');
+   img = loadImage('../images/car.png');
+};
+
+
+function setup() {
+  const canvas = createCanvas(600, 850);
+  canvas.parent('game-board')
+  car = new Car(260, 680, 80, 120);
+};
+
+
+class Car {
+  constructor(x, y, width, height){
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+   
+  };
+
+  draw(){
+    image(img, this.x, this.y, this.width, this.height);
+  };
+
+  moveX(num){
+   this.x += num;
+  };
+
+  moveY(num){
+    this.y += num
+  }
+}
+
+function draw() {
+ background(bg);
+ car.draw();
+ if(keyIsDown(LEFT_ARROW)){
+   car.moveX(-10)
+ } else if(keyIsDown(RIGHT_ARROW)){
+   car.moveX(10)
+ };
+
+ //ran out of time to do anything cooler 🥲
+};
+ 
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    splashScreen.getElementsByClassName.display = 'none'
+    
+    
+  }
 };
+
